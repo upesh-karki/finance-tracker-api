0593ac9de32ede45fdd5a75dfd7ab23224259c14
upace <upacekar3@gmail.com>
2024-12-27 18:48:53 -0500

    Initial commit

---
+++ src/main/java/com/upkdev/FinancialTrackerAPI/FinancialTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.upkdev.FinancialTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upkdev/FinancialTrackerAPI/FinancialTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upkdev.FinancialTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
